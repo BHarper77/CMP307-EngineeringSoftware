bc3beb9c565f64143506dc6ba28b2b4327e1e920
BHarper77 <58574645+BHarper77@users.noreply.github.com>
2020-01-20 12:50:59 +0000

    Add files via upload

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class UserIDClass
+    {
+        int userID = 0;
+
+        //Getter and setter for passing UserID between forms
+        public void setter(int ID)
+        {
+            ID = userID;
+        }
+
+        public int getter()
+        {
+            return userID;
+        }
+    }
+}
